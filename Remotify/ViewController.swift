6233622fad52538e9ba2a93703a854ea2d4c4f58
Salil Thakur <salil123@gmail.com>
2017-08-14 21:47:37 -0400

    Initial Commit

---
+++ Remotify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Remotify
+//
+//  Created by Salil Thakur on 8/14/17.
+//  Copyright © 2017 Salil Thakur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
